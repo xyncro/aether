2311e81d86bb0b24a1a3775493c0bd7ac05e9c8a
kolektiv <andrew@xyncro.com>
2014-09-23 23:48:44 +0100

    some work on making isomorphisms a type with different compositional logic

--- src/Aether/Aether.fs
+++ src/Aether/Aether.fs
@@ -1,5 +1,6 @@
 ﻿namespace Aether
 
+// Lenses
 
 /// Lens of get/set functions which will always succeed
 type Lens<'a,'b> = ('a -> 'b) * ('b -> 'a -> 'a)
@@ -7,126 +8,110 @@ type Lens<'a,'b> = ('a -> 'b) * ('b -> 'a -> 'a)
 /// Partial lens of get/set functions which may not succeed
 type PLens<'a,'b> = ('a -> 'b option) * ('b -> 'a -> 'a)
 
+// Isomorphisms
+
+/// Isomorphism of a <> b which will always succeed
+type Iso<'a,'b> = ('a -> 'b) * ('b -> 'a)
+
+/// Isomorphism of a <> b which may not succeed
+type PIso<'a,'b> = ('a -> 'b option) * ('b -> 'a)
+
 
 [<AutoOpen>]
 module Functions =
 
-    let internal get (g, _) = g
-    let internal set (_, s) = s
-    
     /// Get a value using a lens
-    let getL ((g, _): Lens<'a,'b>) = g
+    let getL ((g, _): Lens<'a,'b>) = 
+        g
 
     /// Get a value option using a partial lens
-    let getPL ((g, _): PLens<'a,'b>) = g
+    let getPL ((g, _): PLens<'a,'b>) = 
+        g
 
     /// Get a value or a default using a partial lens
-    let getOrPL ((g, _): PLens<'a,'b>) (b: 'b) = fun a -> g a |> function | Some b -> b | _ -> b
+    let getOrPL ((g, _): PLens<'a,'b>) = 
+        fun b a -> g a |> function | Some b -> b | _ -> b
 
     /// Set a value using a lens
-    let setL ((_, s): Lens<'a,'b>) = s
+    let setL ((_, s): Lens<'a,'b>) =
+        s
 
     /// Set a value using a partial lens
-    let setPL ((_, s): PLens<'a,'b>) = s
+    let setPL ((_, s): PLens<'a,'b>) =
+        s
 
     /// Modify a value using a lens
-    let modL ((g, s): Lens<'a,'b>) = fun f a -> s (f (g a)) a
+    let modL ((g, s): Lens<'a,'b>) = 
+        fun f a -> s (f (g a)) a
 
     /// Modify a value using a partial lens
-    let modPL ((g, s): PLens<'a,'b>) = fun f a -> Option.map f (g a) |> function | Some b -> s b a | _ -> a
+    let modPL ((g, s): PLens<'a,'b>) = 
+        fun f a -> Option.map f (g a) |> function | Some b -> s b a | _ -> a
 
 
 [<AutoOpen>]
-module Compositors =
+module Composition =
 
-    let internal composeGet l1 l2 =
-        fun a -> get l2 (get l1 a)
+    // Lenses with Lenses
 
-    let internal composePartialTotalGet l1 l2 =
-        fun a -> Option.map (get l2) (get l1 a)
-
-    let internal composePartialPartialGet l1 l2 =
-        fun a -> Option.bind (get l2) (get l1 a)
-
-    let internal composeSet l1 l2 =
-        fun c a -> set l1 (set l2 c (get l1 a)) a
-
-    let internal composePartialSet l1 l2 =
-        fun c a -> Option.map (set l2 c) (get l1 a) |> function | Some b -> set l1 b a | _ -> a
-
-    /// Compose two lenses
-    let composeTT (l1: Lens<'a,'b>) (l2: Lens<'b,'c>) : Lens<'a,'c> =
-        composeGet l1 l2, composeSet l1 l2
+    /// Compose a total lens and a total lens, giving a total lens
+    let composeLL ((g1, s1): Lens<'a,'b>) ((g2, s2): Lens<'b,'c>) : Lens<'a,'c> =
+        ((fun a -> g2 (g1 a)),
+         (fun c a -> s1 (s2 c (g1 a)) a))
 
     /// Compose a lens and a partial lens, giving a partial lens
-    let composeTP (l1: Lens<'a,'b>) (l2: PLens<'b,'c>) : PLens<'a,'c> =
-        composeGet l1 l2, composeSet l1 l2
+    let composeLPL ((g1, s1): Lens<'a,'b>) ((g2, s2): PLens<'b,'c>) : PLens<'a,'c> =
+        ((fun a -> g2 (g1 a)),
+         (fun c a -> s1 (s2 c (g1 a)) a))
 
     /// Compose a partial lens and a lens, giving a partial lens
-    let composePT (l1: PLens<'a,'b>) (l2: Lens<'b,'c>) : PLens<'a,'c> =
-        composePartialTotalGet l1 l2, composePartialSet l1 l2
+    let composePLL ((g1, s1): PLens<'a,'b>) ((g2, s2): Lens<'b,'c>) : PLens<'a,'c> =
+        ((fun a -> Option.map g2 (g1 a)),
+         (fun c a -> Option.map (s2 c) (g1 a) |> function | Some b -> s1 b a | _ -> a))
 
     /// Compose two partial lenses, giving a partial lens
-    let composePP (l1: PLens<'a,'b>) (l2: PLens<'b,'c>) : PLens<'a,'c> =
-        composePartialPartialGet l1 l2, composePartialSet l1 l2
+    let composePLPL ((g1, s1): PLens<'a,'b>) ((g2, s2): PLens<'b,'c>) : PLens<'a,'c> =
+        ((fun a -> Option.bind g2 (g1 a)),
+         (fun c a -> Option.map (s2 c) (g1 a) |> function | Some b -> s1 b a | _ -> a))
 
+    // Lenses with Isomorphisms
 
-[<AutoOpen>]
-module Lenses =
+    /// Compose a total lens with a total isomorphism, giving a total lens
+    let composeLI ((g, s): Lens<'a,'b>) ((f, t): Iso<'b,'c>) : Lens<'a,'c> =
+        ((fun a -> f (g a)),
+         (fun c a -> s (t c) a))
 
-    /// Isomorphism giving a lens
-    let isoLens (ab: 'a -> 'b) (ba: 'b -> 'a) : Lens<'a,'b> =
-        ab, (fun b _ -> ba b)
-
-    /// Partial isomorphism giving a partial lens
-    let isoPLens (ab: 'a -> 'b option) (ba: 'b -> 'a) : PLens<'a, 'b> =
-        ab, (fun b _ -> ba b)
-
-    /// First item of a tuple giving a lens
-    let fstLens (t: 'a * 'b) : Lens<('a * 'b),'a> =
-        fst, (fun a t -> a, snd t)
-        
-    /// Second item of a tuple giving a lens
-    let sndLens (t: 'a * 'b) : Lens<('a * 'b),'b> =
-        snd, (fun b t -> fst t, b)
-
-    /// Head of a list giving a partial lens
-    let headPLens : PLens<'v list, 'v> =
-        (function | h :: t -> Some h | _ -> None),
-        (fun v -> function | h :: t -> v :: t | l -> l)
-
-    /// Position of a list giving a partial lens
-    let listPLens (i: int) : PLens<'v list, 'v> =
-        (function | l when List.length l > i -> Some (List.nth l i) | _ -> None), 
-        (fun v l -> List.mapi (fun i' x -> if i = i' then v else x) l)
-
-    /// Tail of a list giving a partial lens
-    let tailPLens : PLens<'v list, 'v list> =
-        (function | h :: t -> Some t | _ -> None),
-        (fun t -> function | h :: _ -> h :: t | l -> l)
-
-    /// Key of a map giving a partial lens
-    let mapPLens (k: 'k) : PLens<Map<'k,'v>,'v> =
-        Map.tryFind k, Map.add k
+    /// Compose a partial lens with a total isomorphism, giving a partial lens
+    let composePLI ((g, s): PLens<'a,'b>) ((f, t): Iso<'b, 'c>) : PLens<'a,'c> =
+        ((fun a -> Option.map f (g a)),
+         (fun c a -> s (t c) a))
 
 
 module Operators =
 
-    /// Compose two lenses
+    /// Compose a total lens and a total lens, giving a total lens
     let (>-->) l1 l2 =
-        composeTT l1 l2
+        composeLL l1 l2
 
-    /// Compose a lens and a partial lens, giving a partial lens
+    /// Compose a total lens and a partial lens, giving a partial lens
     let (>-?>) l1 l2 =
-        composeTP l1 l2
+        composeLPL l1 l2
 
-    /// Compose a partial lens and a lens, giving a partial lens
+    /// Compose a partial lens and a total lens, giving a partial lens
     let (>?->) l1 l2 =
-        composePT l1 l2
+        composePLL l1 l2
 
     /// Compose two partial lenses, giving a partial lens
     let (>??>) l1 l2 =
-        composePP l1 l2
+        composePLPL l1 l2
+
+    /// Compose a total lens with a total isomorphism, giving a total lens
+    let (<-->) l i =
+        composeLI l i
+
+    /// Compose a partial lens with a total isomorphism, giving a partial lens
+    let (<?->) l i =
+        composePLI l i
 
 
     module Infix =
@@ -154,3 +139,34 @@ module Operators =
         /// Modify a value using a partial lens
         let (^?%=) (l: PLens<'a,'b>) (f: 'b -> 'b) : 'a -> 'a =
             modPL l f
+
+
+[<AutoOpen>]
+module Lenses =
+
+    /// First item of a tuple giving a lens
+    let fstLens (_: 'a * 'b) : Lens<('a * 'b),'a> =
+        fst, (fun a t -> a, snd t)
+        
+    /// Second item of a tuple giving a lens
+    let sndLens (_: 'a * 'b) : Lens<('a * 'b),'b> =
+        snd, (fun b t -> fst t, b)
+
+    /// Head of a list giving a partial lens
+    let headPLens : PLens<'v list, 'v> =
+        (function | h :: _ -> Some h | _ -> None),
+        (fun v -> function | _ :: t -> v :: t | l -> l)
+
+    /// Position of a list giving a partial lens
+    let listPLens (i: int) : PLens<'v list, 'v> =
+        (function | l when List.length l > i -> Some (List.nth l i) | _ -> None), 
+        (fun v l -> List.mapi (fun i' x -> if i = i' then v else x) l)
+
+    /// Tail of a list giving a partial lens
+    let tailPLens : PLens<'v list, 'v list> =
+        (function | _ :: t -> Some t | _ -> None),
+        (fun t -> function | h :: _ -> h :: t | l -> l)
+
+    /// Key of a map giving a partial lens
+    let mapPLens (k: 'k) : PLens<Map<'k,'v>,'v> =
+        Map.tryFind k, Map.add k
---
+++ tests/Aether.Tests/Aether.Tests.fs
@@ -0,0 +1,80 @@
+﻿namespace Aether.Tests
+
+open System
+open Aether
+open Aether.Operators
+open NUnit.Framework
+open Swensen.Unquote
+
+
+[<AutoOpen>]
+module Data =
+
+    type First =
+        { A: string
+          B: string option }
+
+    let firstALens : Lens<First, string> =
+        ((fun x -> x.A),
+         (fun a x -> { x with A = a }))
+
+    let firstBPLens : PLens<First, string> =
+        ((fun x -> x.B),
+         (fun b x -> { x with B = Some b }))
+
+    let f1 =
+        { A = "a"
+          B = Some "b" }
+
+    let f2 =
+        { A = "a"
+          B = None }
+
+
+module Functions =
+
+    [<Test>]
+    let ``getL returns correct values`` () =
+        getL firstALens f1 =? "a"
+
+    [<Test>]
+    let ``getPL returns correct values`` () =
+        getPL firstBPLens f1 =? Some "b"
+
+    [<Test>]
+    let ``setL sets value correctly`` () =
+        setL firstALens "aa" f1 |> fun x -> x.A =? "aa"
+
+    [<Test>]
+    let ``setPL sets value correctly`` () =
+        setPL firstBPLens "bb" f1 |> fun x -> x.B =? Some "bb"
+
+    [<Test>]
+    let ``modL modifies values correctly`` () =
+        modL firstALens (fun x -> x + x) f1 |> fun x -> x.A =? "aa"
+
+    [<Test>]
+    let ``modPL modifies values correctly`` () =
+        modPL firstBPLens (fun x -> x + x) f1 |> fun x -> x.B =? Some "bb"
+
+module Isomorphisms =
+
+    let lens = 
+        firstBPLens 
+        <?-> ((fun x -> x.ToCharArray ()), (fun x -> String (x)))
+
+    [<Test>]
+    let ``isomorphism over partial return value when Some`` () =
+        getPL lens f1 =? Some [| 'b' |]
+
+    [<Test>]
+    let ``isomorphism over partial return None when None`` () =
+        getPL lens f2 =? None
+
+    [<Test>]
+    let ``isomorphism over partial sets value when Some`` () =
+        setPL lens [| 'b'; 'b' |] f1 |> fun x -> x.B =? Some "bb"
+
+    [<Test>]
+    let ``isomorphism over partial sets value when None`` () =
+        setPL lens [| 'b'; 'b' |] f2 |> fun x -> x.B =? Some "bb"
