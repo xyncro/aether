19a9e0538665deed21b2ca8204e4dcb67803078a
Justin <justin@agnomendesign.com>
2015-07-10 12:54:41 +0200

    Moved to Fsharp project scaffold

---
+++ src/Aether/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Aether")>]
+[<assembly: AssemblyProductAttribute("Aether")>]
+[<assembly: AssemblyDescriptionAttribute("Total/Partial Lenses for F#")>]
+[<assembly: AssemblyVersionAttribute("6.1.0")>]
+[<assembly: AssemblyFileVersionAttribute("6.1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "6.1.0"
