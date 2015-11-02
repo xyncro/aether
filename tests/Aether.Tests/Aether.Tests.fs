813ebbbf89e03f1ea9f394e7566e28e5b1cbc11f
Marcus Griep <mgriep@cimpress.com>
2015-11-02 12:56:44 -0500

    Switch from Fuchu to xUnit

--- tests/Aether.Tests/Aether.Tests.fs
+++ tests/Aether.Tests/Aether.Tests.fs
@@ -5,6 +5,7 @@ open Aether
 open Aether.Operators
 open Fuchu
 open FsCheck
+open FsCheck.Xunit
 open Swensen.Unquote
 
 [<AutoOpen>]
@@ -18,6 +19,42 @@ module Data =
       (fun v x -> match x with | Choice2Of2 _ -> Choice2Of2 v | _ -> x))
 
 module Lens =
+  let getSetIdentityPropertyWith lensGet lensSet outer =
+    "Get-Set Identity" @| lazy
+      test <@ lensSet (lensGet outer) outer = outer @>
+
+  let setGetSymmetryPropertyWith lensGet lensSet outer inner =
+    "Set-Get Symmetry" @| lazy
+      test <@ lensGet (lensSet inner outer) = inner @>
+
+  let setSetOrderDependencePropertyWith lensSet outer inner dummy =
+    "Set-Set Order Dependence" @| lazy
+      test <@ lensSet inner (lensSet dummy outer) = lensSet inner outer @>
+    |> Prop.trivial (inner = dummy)
+      
+  let getSetMapCorrespondencePropertyWith lensGet lensSet lensMap f outer =
+    "Get-Set to Map Correspondence" @| lazy
+      test <@ lensSet (lensGet outer |> f) outer = lensMap f outer @>
+
+  let inline getSetIdentityProperty lens =
+    getSetIdentityPropertyWith (Lens.get lens) (Lens.set lens)
+
+  let inline setGetSymmetryProperty lens =
+    setGetSymmetryPropertyWith (Lens.get lens) (Lens.set lens)
+
+  let inline setSetOrderDependenceProperty lens =
+    setSetOrderDependencePropertyWith (Lens.set lens)
+
+  let inline getSetMapCorrespondenceProperty lens =
+    getSetMapCorrespondencePropertyWith (Lens.get lens) (Lens.set lens) (Lens.map lens)
+
+  let followsLensLawsProperty lens outer inner dummy f =
+    getSetIdentityProperty lens outer .&.
+    setGetSymmetryProperty lens outer inner .&.
+    setSetOrderDependenceProperty lens outer inner dummy .&.
+    getSetMapCorrespondenceProperty lens f outer
+    |> Prop.trivial (inner = dummy)
+
   let inline getSetIdentityWith lensName lensGet lensSet =
     testProperty (sprintf "%s lens demonstrates Get-Set identity" lensName) <| fun outer ->
       test <@ lensSet (lensGet outer) outer = outer @>
@@ -61,6 +98,58 @@ module Lens =
     followsLensLawsWithName lensName lens
 
 module Prism =
+  let classifyInner innerP =
+    Prop.classify (Option.isSome innerP) "has inner"
+    >> Prop.classify (Option.isNone innerP) "no inner"
+
+  let inline classifyInnerWith prismGet outer =
+    classifyInner (prismGet outer)
+
+  let inline getSetIdentityPropertyWith prismGet prismSet outer dummy =
+    "Get-Set Identity" @| lazy
+      test <@ prismSet (defaultArg (prismGet outer) dummy) outer = outer @>
+    |> classifyInnerWith prismGet outer
+
+  let inline setGetSymmetryPropertyWith prismGet prismSet outer inner =
+    "Set-Get Symmetry" @| lazy
+      match prismGet outer with
+      | Some _ -> "inner should be changed" @| (test <@ prismSet inner outer |> prismGet = Some inner @>)
+      | None -> "outer should remain unchanged" @| (test <@ prismSet inner outer = outer @>)
+    |> classifyInnerWith prismGet outer
+
+  let inline setSetOrderDependencePropertyWith prismGet prismSet outer inner dummy =
+    "Set-Set Order Dependence" @| lazy
+      test <@ prismSet inner outer = (prismSet inner (prismSet dummy outer)) @>
+    |> classifyInnerWith prismGet outer
+    |> Prop.trivial (inner = dummy)
+
+  let inline getSetMapCorrespondencePropertyWith prismGet prismSet prismMap f outer =
+    "Get-Set to Map Correspondence" @| lazy
+      test <@ prismMap f outer = (prismGet outer |> function | Some i -> prismSet (f i) outer | None -> outer) @>
+    |> classifyInnerWith prismGet outer
+
+  let inline unwrapPrism f prism =
+    f (Lens.getPartial prism) (Lens.setPartial prism)
+
+  let inline getSetIdentityProperty prism =
+    unwrapPrism getSetIdentityPropertyWith prism
+
+  let inline setGetSymmetryProperty prism =
+    unwrapPrism setGetSymmetryPropertyWith prism
+
+  let inline setSetOrderDependenceProperty prism =
+    unwrapPrism setSetOrderDependencePropertyWith prism
+
+  let inline getSetMapCorrespondenceProperty prism =
+    unwrapPrism getSetMapCorrespondencePropertyWith prism (Lens.mapPartial prism)
+
+  let followsPrismLawsProperty prism outer inner dummy f =
+    getSetIdentityProperty prism outer dummy .&.
+    setGetSymmetryProperty prism outer inner .&.
+    setSetOrderDependenceProperty prism outer inner dummy .&.
+    getSetMapCorrespondenceProperty prism f outer
+    |> Prop.trivial (inner = dummy)
+
   let classifyTarget prismGet outer =
     let innerP = prismGet outer 
     Prop.classify (Option.isSome innerP) "has inner"
@@ -134,6 +223,44 @@ module Iso =
     let isoName = isoQ.Decompile()
     let iso = isoQ.Eval()
     followsIsoLawsWithName isoName iso
+
+module ``Built-in Lenses`` =
+  [<Property>]
+  let ``id_ follows the Lens Laws`` (outer : obj, inner, dummy, f) =
+    Lens.followsLensLawsProperty id_ outer inner dummy f
+
+  [<Property>]
+  let ``fst_ follows the Lens Laws`` (outer : obj * obj, inner, dummy, f) =
+    Lens.followsLensLawsProperty fst_ outer inner dummy f
+
+  [<Property>]
+  let ``snd_ follows the Lens Laws`` (outer : obj * obj, inner, dummy, f) =
+    Lens.followsLensLawsProperty snd_ outer inner dummy f
+
+module ``Built-in Prisms`` =
+  [<Property>]
+  let ``choice1Of2_ follows the Prism Laws`` (outer : Choice<obj,obj>, inner, dummy, f) =
+    Prism.followsPrismLawsProperty choice1Of2_ outer inner dummy f
+
+  [<Property>]
+  let ``choice2Of2_ follows the Prism Laws`` (outer : Choice<obj,obj>, inner, dummy, f) =
+    Prism.followsPrismLawsProperty choice2Of2_ outer inner dummy f
+
+  [<Property>]
+  let ``head_ follows the Prism Laws`` (outer : obj list, inner, dummy, f) =
+    Prism.followsPrismLawsProperty head_ outer inner dummy f
+
+  [<Property>]
+  let ``tail_ follows the Prism Laws`` (outer : obj list, inner, dummy, f) =
+    Prism.followsPrismLawsProperty tail_ outer inner dummy f
+
+  [<Property>]
+  let ``pos_ follows the Prism Laws`` (idx : NonNegativeInt, outer : obj list, inner, dummy, f) =
+    Prism.followsPrismLawsProperty (pos_ idx.Get) outer inner dummy f
+
+  [<Property>]
+  let ``key_ follows the Prism Laws`` (key, outer : Map<string,obj>, inner, dummy, f) =
+    Prism.followsPrismLawsProperty (key_ key) outer inner dummy f
 
 [<Tests>]
 let lensLawTests =
